7c51393a18209f4ba08fb7bdacdc03c5f92d732b
Mira <kruzinka@yandex.ru>
2021-02-19 12:13:58 +0300

    Add DynamicArraySizeCheck and Tests

--- hw12022021/src/DynamicArrayHW/DynamicArray.java
+++ hw12022021/src/DynamicArrayHW/DynamicArray.java
@@ -4,9 +4,23 @@ public class DynamicArray <T> {
 
     private int DEFAULT_SIZE = 15;
 
-    private int sizeArr;
     private T[] dataArr;
 
-    
+    public DynamicArray() {
+        dataArr = (T[]) new Object[DEFAULT_SIZE];
+    }
 
+    public DynamicArray(int size){
+        if (size > -1)
+            dataArr = (T[]) new Object[size];
+        else
+            throw new RuntimeException("Negative Array Size");
+    }
+
+    public 
+
+
+//    public T getVal(int index) {
+//        if ()
+//    }
 }
---
+++ hw12022021/src/DynamicArrayHW/DynamicArrayTests.java
@@ -0,0 +1,13 @@
+package DynamicArrayHW;
+
+import DynamicArrayHW.DynamicArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DynamicArrayTests extends Assertions {
+
+//    @Test
+//    public void DynamicArray_ {
+//
+//    }
+}
