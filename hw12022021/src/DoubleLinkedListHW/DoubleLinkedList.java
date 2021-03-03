c85144ee6cb8bdecef647ade910bca9e3ccb6b24
Mira <kruzinka@yandex.ru>
2021-03-03 22:46:14 +0300

    added DoubleLinkedList() in class

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
@@ -1,5 +1,13 @@
 package DoubleLinkedListHW;
 
-public class DoubleLinkedList {
+public class DoubleLinkedList <T> {
+    private DoubleLinkedListElement head;
+    private DoubleLinkedListElement tail;
+    private int size;
 
+    public DoubleLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 }
