855ce130f90cbfe9e1a68488e8cb97f2aa7006d3
Mira <kruzinka@yandex.ru>
2021-03-03 22:34:10 +0300

    filled DoubleLinkedListElement

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedListElement.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedListElement.java
@@ -1,5 +1,13 @@
 package DoubleLinkedListHW;
 
-public class DoubleLinkedListElement {
-    
+public class DoubleLinkedListElement <T> {
+    DoubleLinkedListElement previous;
+    T data;
+    DoubleLinkedListElement next;
+
+    public DoubleLinkedListElement(DoubleLinkedListElement Previous, T Data, DoubleLinkedListElement Next) {
+        previous = Previous;
+        data = Data;
+        next = Next;
+    }
 }
