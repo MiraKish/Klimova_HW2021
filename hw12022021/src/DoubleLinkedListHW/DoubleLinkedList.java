7f83c0e98dfae7d90b92f69af1e1abaf0ce01bcc
Mira <kruzinka@yandex.ru>
2021-03-04 22:38:02 +0300

    added addByIndex

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
@@ -3,6 +3,7 @@ package DoubleLinkedListHW;
 public class DoubleLinkedList <T> {
     private DoubleLinkedListElement head;
     private DoubleLinkedListElement tail;
+    private int count = 0;
     private int size;
 
     public DoubleLinkedList() {
@@ -23,6 +24,24 @@ public class DoubleLinkedList <T> {
         return head == null;
     }
 
+    public void getByIndex(int index) {
+        
+    }
 
+    public void addByIndex(int index, T data) {
+        DoubleLinkedListElement oldEl = head;
+
+        while (oldEl != null && count != index) {
+            oldEl = oldEl.next;
+            count++;
+        }
+
+        DoubleLinkedListElement element = new DoubleLinkedListElement(null, data, head);
+
+        oldEl.previous.next = element;
+        element.previous = oldEl.previous;
+        oldEl.previous = oldEl;
+        element.next = oldEl;
+    }
 
 }
