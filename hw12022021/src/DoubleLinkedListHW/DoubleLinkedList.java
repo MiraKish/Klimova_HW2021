655ff5740cfb0de61692f783ca9f2f9b352702a1
Mira <kruzinka@yandex.ru>
2021-03-04 21:29:41 +0300

    added getSize and emptyList

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedList.java
@@ -10,4 +10,19 @@ public class DoubleLinkedList <T> {
         tail = null;
         size = 0;
     }
+
+    public void throwException() {
+        throw new RuntimeException();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int emptyList() {
+        size = 0;
+        return size;
+    }
+
+
 }
