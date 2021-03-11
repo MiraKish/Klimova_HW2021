2e6afdae248d6d9b86e32aa045ec4ad48274cd59
Mira <kruzinka@yandex.ru>
2021-03-11 21:46:53 +0300

    added StackDLL and push()

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/StackDLL.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/StackDLL.java
@@ -1,7 +1,16 @@
 package DoubleLinkedListHW;
 
 public class StackDLL <T> {
+    private DoubleLinkedList<T> dataStack;
+    private T top;
+
     DoubleLinkedList<T> doubleLinkedList = new DoubleLinkedList<>();
+
+    public StackDLL() {
+        DoubleLinkedList<T> doubleLinkedList = new DoubleLinkedList<>();
+        dataStack = doubleLinkedList;
+        top = dataStack.getHead();
+    }
 
     public int getSize() {
         return doubleLinkedList.getSize();
@@ -9,6 +18,10 @@ public class StackDLL <T> {
 
     public boolean emptyStack() {
         return doubleLinkedList.emptyList();
+    }
+
+    public void push(T data) {
+        top = data;
     }
 
     
