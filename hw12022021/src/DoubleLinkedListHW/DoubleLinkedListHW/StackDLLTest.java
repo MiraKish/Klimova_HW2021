8f4a1250f09c99fc308fbcbe88ae685b93b814dc
Mira <kruzinka@yandex.ru>
2021-03-11 22:39:34 +0300

    puch exception test

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/StackDLLTest.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/StackDLLTest.java
@@ -8,10 +8,27 @@ public class StackDLLTest extends Assertions  {
     @Test
     public void push_addsNewPick_tailEquals6() {
 
-        StackDLL<Integer> stackDLL = new StackDLL<>();
+        StackDLL<Integer> stackDLL = new StackDLL<Integer>();
 
         stackDLL.push(6);
 
         assertEquals(6, stackDLL.peek());
+    }
+
+    @Test
+    public void push_tooManyElements_throwsException() {
+
+        StackDLL<Integer> stackDLL = new StackDLL<Integer>();
+
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+        stackDLL.push(6);
+
+        assertThrows(RuntimeException.class, () -> stackDLL.push(7));
     }
 }
