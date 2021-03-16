5f24d0bf4bde4144b790b7ede05bb486530c638e
Mira <kruzinka@yandex.ru>
2021-03-16 21:29:36 +0300

    getSize added

--- hw12022021/src/DynamicArrayHW/DynamicArrayHW/Queue.java
+++ hw12022021/src/DynamicArrayHW/DynamicArrayHW/Queue.java
@@ -1,5 +1,21 @@
 package DynamicArrayHW;
 
-public class Queue {
-    
-}
+public class Queue <T> {
+    DynamicArray<T> dynamicArray = new DynamicArray<>();
+
+//    private String  = " ";
+//    private String  = " ";
+//
+//    private void throws() {
+//        throw new RuntimeException();
+//    }
+//
+//    private void throws() {
+//        throw new RuntimeException();
+//}
+
+    public int getSize() {
+        return dynamicArray.size();
+    }
+
+    public
