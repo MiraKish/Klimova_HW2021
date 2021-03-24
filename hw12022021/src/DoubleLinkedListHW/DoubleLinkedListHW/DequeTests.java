2362f2c100071220fbe4ae1ea59e83b42f76bab7
Mira <kruzinka@yandex.ru>
2021-03-24 21:43:14 +0300

    Task 6 complete (added final tests)

--- hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/DequeTests.java
+++ hw12022021/src/DoubleLinkedListHW/DoubleLinkedListHW/DequeTests.java
@@ -119,4 +119,37 @@ public class DequeTests extends Assertions {
 
         assertEquals(2, deque.getSize());
     }
+
+    @Test
+    public void peekFront_deletesAndAddsRight_head6() {
+
+        Deque<String> deque = new Deque<>();
+
+        deque.pushFront("6");
+        deque.pushFront("8");
+        deque.pushBack("7");
+        deque.pushBack("9");
+        deque.popFront();
+        deque.popBack();
+        deque.popBack();
+        deque.pushBack("10");
+
+        assertEquals("6", deque.peekFront());
+    }
+
+    @Test
+    public void peekBack_deletesAndAddsRight_tail10() {
+
+        Deque<String> deque = new Deque<>();
+
+        deque.pushFront("6");
+        deque.pushFront("8");
+        deque.pushBack("7");
+        deque.pushBack("9");
+        deque.popFront();
+        deque.popBack();
+        deque.pushBack("10");
+
+        assertEquals("10", deque.peekBack());
+    }
 }
