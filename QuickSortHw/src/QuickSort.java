42b02ade86d8a12d4ac93cf0f82ce146be635c60
Mira <kruzinka@yandex.ru>
2021-04-02 11:49:22 +0300

    added code from NLogNSort

--- QuickSortHw/src/QuickSort.java
+++ QuickSortHw/src/QuickSort.java
@@ -1,2 +1,8 @@
+import java.util.Arrays;
+
 public class QuickSort {
+    public void sort(int[] data)
+    {
+        Arrays.sort(data); // Replace this
+    }
 }
--- QuickSortHw/src/QuickSortTest.java
+++ QuickSortHw/src/QuickSortTest.java
@@ -1,2 +1,65 @@
-public class QuickSortTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QuickSortTest extends Assertions {
+    @Test
+    public void sort_emptyArray_doNothing()
+    {
+        int[] actual = new int[0];
+        int[] expected = new int[0];
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sort_oneElementArray_doNothing()
+    {
+        int[] actual = { 10 };
+        int[] expected = { 10 };
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sort_allElementsEquals_doNothing()
+    {
+        int[] actual = { 10, 10, 10, 10, 10 };
+        int[] expected = { 10, 10, 10, 10, 10 };
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sort_allElementsDifferent_correctSorting()
+    {
+        int[] actual = { 10, 1, 5, 11, 2, 6, 8, 4, 9, 3, 7 };
+        int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sort_containsSomeEquals_correctSorting()
+    {
+        int[] actual = { 10, 1, 5, 5, 11, 2, 5, 6, 8, 1, 4, 2, 10, 9, 3, 7, 10, 1, 2 };
+        int[] expected = { 1, 1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10, 10, 10, 11 };
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sort_alreadySorted_doNothing()
+    {
+        int[] actual = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        QuickSort sort = new QuickSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
 }
