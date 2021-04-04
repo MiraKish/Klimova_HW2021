fde8eb03675a2c6a777140926e32fbe625d7eb84
Mira <kruzinka@yandex.ru>
2021-04-04 21:11:27 +0300

    added quickSort for dividing

--- QuickSortHw/src/NLogNSort.java
+++ QuickSortHw/src/NLogNSort.java
@@ -2,5 +2,31 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class NLogNSort {
-    
+    private int[] numbers;
+    private int number;
+
+    private void quicksort(int low, int high) {
+        int i = low, j = high;
+
+        int pivot = numbers[low + (high-low)/2];
+
+        // Dividing
+        while (i <= j) {
+            while (numbers[i] < pivot) {
+                i++;
+            }
+
+            while (numbers[j] > pivot) {
+                j--;
+            }
+
+            //
+        }
+        // Recursion
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
+
 }
