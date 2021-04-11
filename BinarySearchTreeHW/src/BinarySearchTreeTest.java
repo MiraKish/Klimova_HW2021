9201f8396f2d0fbe651e223cf1680bc16c037201
Mira <kruzinka@yandex.ru>
2021-04-11 21:24:21 +0300

    added test for insert

--- BinarySearchTreeHW/src/BinarySearchTreeTest.java
+++ BinarySearchTreeHW/src/BinarySearchTreeTest.java
@@ -2,5 +2,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BinarySearchTreeTest extends Assertions {
-    
+    @Test
+    public void insert_addsNewValue_value8EqualsTrue() {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert( 8);
+
+        assertEquals(true, tree.search(8));
+    }
+
+
 }
