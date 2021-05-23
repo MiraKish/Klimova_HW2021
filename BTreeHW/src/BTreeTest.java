5eba942b1f88d596dde3ce90993c540f76d4dfe7
Mira <kruzinka@yandex.ru>
2021-05-23 11:53:15 +0300

    added project for BTree

---
+++ BTreeHW/src/BTreeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BTreeTest extends Assertions {
+    @Test
+    public void bruh() {
+        BTree tree = new BTree();
+
+
+        assertEquals();
+    }
+}
