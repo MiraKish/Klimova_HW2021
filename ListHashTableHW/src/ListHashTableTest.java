026d6dbf2ec59b347f656dd1a659018595b79b08
Mira <kruzinka@yandex.ru>
2021-05-23 12:36:50 +0300

    added project for ListHashTable

---
+++ ListHashTableHW/src/ListHashTableTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ListHashTableTest extends Assertions {
+//    @Test
+//    public void bruh() {
+//        BTree tree = new BTree();
+//
+//
+//        assertEquals();
+//    }
+}
