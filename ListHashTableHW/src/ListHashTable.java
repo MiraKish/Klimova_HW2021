10b66840f140393fce3fa83f26928e64bcddafee
Mira <kruzinka@yandex.ru>
2021-05-23 13:16:37 +0300

    added search and test for insert

--- ListHashTableHW/src/ListHashTable.java
+++ ListHashTableHW/src/ListHashTable.java
@@ -1,3 +1,43 @@
 public class ListHashTable {
+    int[] arr;
+    int capacity;
 
+    public ListHashTable(int capacity) {
+        this.capacity = nextPrime(capacity);
+        arr = new int[this.capacity];
+    }
+
+    // Function to check if given number is prime **/
+    private static boolean isPrime(int n) {
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n == 1 || n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Function to generate next prime number >= n
+    private static int nextPrime(int n) {
+        if (n % 2 == 0) {
+            n++;
+        }
+
+        for (; !isPrime(n); n += 2);
+
+        return n;
+    }
+
+    public void insert(int elem) {
+        arr[elem % capacity] = elem;
+    }
+
+    public boolean search(int ele) {
+        return arr[ele % capacity] == ele;
+    }
 }
--- ListHashTableHW/src/ListHashTableTest.java
+++ ListHashTableHW/src/ListHashTableTest.java
@@ -2,11 +2,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ListHashTableTest extends Assertions {
-//    @Test
-//    public void bruh() {
-//        BTree tree = new BTree();
-//
-//
-//        assertEquals();
-//    }
+    @Test
+    public void insert_element8_equalsTrue() {
+        ListHashTable table = new ListHashTable(10);
+
+        table.insert(4);
+        table.insert(11);
+        table.insert(8);
+
+        assertEquals(true, table.search(11));
+    }
+
+
 }
