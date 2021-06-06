d8d73106b3f8f604eef83b1ea3c06b4300bc1e8a
Mira <kruzinka@yandex.ru>
2021-06-06 14:58:56 +0300

    added Node and Edge classes

--- Graphs/src/code/EdgesListGraph.java
+++ Graphs/src/code/EdgesListGraph.java
@@ -1,5 +1,43 @@
 package code;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class EdgesListGraph {
-    
+    public static class Node {
+        private int num;
+        private LinkedList < Edge > edgeList;
+
+        public Node(int num) {
+            this.num = num;
+            edgeList = new LinkedList < > ();
+        }
+
+        public LinkedList < Edge > getEdges() {
+            return edgeList;
+        }
+    }
+
+    public static class Edge {
+        private int weight;
+        private Node destVertex;
+
+        public Edge(Node dest, int w) {
+            this.destVertex = dest;
+            this.weight = w;
+        }
+
+        public Edge(Node dest) {
+            this.destVertex = dest;
+            this.weight = 1;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public Node getDestVertex() {
+            return destVertex;
+        }
+    }
 }
