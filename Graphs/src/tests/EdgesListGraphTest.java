00d423aa7edbb1ee7882613425f82927cef05a14
Mira <kruzinka@yandex.ru>
2021-06-06 15:02:50 +0300

    Task_280521 complete (added test for removeEdge)

--- Graphs/src/tests/EdgesListGraphTest.java
+++ Graphs/src/tests/EdgesListGraphTest.java
@@ -18,4 +18,17 @@ public class EdgesListGraphTest extends Assertions {
 
         assertEquals(true, graph.AddEdge(v0, v1, 2));
     }
+
+    @Test
+    public void removeEdge_removesEdge_oldEdgeEqualsFalse() {
+        EdgesListGraph.Graph graph = new EdgesListGraph.Graph();
+
+        EdgesListGraph.Node v0 = new EdgesListGraph.Node(0);
+        EdgesListGraph.Node v1 = new EdgesListGraph.Node(1);
+
+        graph.AddVertex(v0);
+        graph.AddVertex(v1);
+
+        assertEquals(false, graph.RemoveEdge(v0, v1, 2));
+    }
 }
