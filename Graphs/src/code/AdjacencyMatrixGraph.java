4857537ab9401b05ebbbdc8aae1be5501fe8a19c
Mira <kruzinka@yandex.ru>
2021-06-06 13:08:45 +0300

    added exception

--- Graphs/src/code/AdjacencyMatrixGraph.java
+++ Graphs/src/code/AdjacencyMatrixGraph.java
@@ -4,6 +4,12 @@ public class AdjacencyMatrixGraph {
     private boolean adjMatrix[][];
     private int numVertices;
 
+    private String outOfBorders = "Your edges are out of borders!";
+
+    private void throwsOutOfBordersExc() {
+        throw new RuntimeException(outOfBorders);
+    }
+
     // Initialize the matrix
     public AdjacencyMatrixGraph(int numVertices) {
         this.numVertices = numVertices;
@@ -12,6 +18,9 @@ public class AdjacencyMatrixGraph {
 
     // Add edges
     public void addEdge(int i, int j) {
+        if (i > numVertices || j > numVertices) {
+            throwsOutOfBordersExc();
+        }
         adjMatrix[i][j] = true;
         adjMatrix[j][i] = true;
     }
