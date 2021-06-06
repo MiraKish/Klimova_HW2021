6c33aaf05ac0bf4a57175ea6b731a42a9283e549
Mira <kruzinka@yandex.ru>
2021-06-06 14:14:58 +0300

    added AdjacencyListGraph()

--- Graphs/src/code/AdjacencyListGraph.java
+++ Graphs/src/code/AdjacencyListGraph.java
@@ -1,5 +1,16 @@
 package code;
 
-public class AdjacencyListGraph {
+import java.util.*;
 
+public class AdjacencyListGraph {
+    int n;
+    LinkedList<LinkedList<Integer> > adj;
+
+    public AdjacencyListGraph(int n0) {
+        n = n0;
+        adj = new LinkedList<LinkedList<Integer>>();
+        for (int i = 0; i < n; ++i) {
+            adj.add(new LinkedList<Integer>());
+        }
+    }
 }
